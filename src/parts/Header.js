d58bc4a17a5d36df0dea7a084f3cd8da5790030e
elroypedrokameo <elroypedrokameo10@gmail.com>
2023-04-25 05:10:05 +0700

    add react reveal

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
+
 import Button from "elements/Button";
 import BrandIcon from "parts/IconText";
 
@@ -8,38 +10,40 @@ export default function Header(props) {
   };
 
   return (
-    <header className="spacing-sm">
-      <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light">
-          <div className="container-fluid">
-            <BrandIcon />
-            <div className="collapse navbar-collapse flex-grow-0">
-              <ul className="navbar-nav ml-auto">
-                <li className={`nav-item${getNavLinkClass("/")}`}>
-                  <Button className="nav-link" type="link" href="/">
-                    Home
-                  </Button>
-                </li>
-                <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
-                  <Button className="nav-link" type="link" href="browse-by">
-                    Browse by
-                  </Button>
-                </li>
-                <li className={`nav-item${getNavLinkClass("/stories")}`}>
-                  <Button className="nav-link" type="link" href="/stories">
-                    Stories
-                  </Button>
-                </li>
-                <li className={`nav-item${getNavLinkClass("/agents")}`}>
-                  <Button className="nav-link" type="link" href="/agents">
-                    Agents
-                  </Button>
-                </li>
-              </ul>
+    <Fade>
+      <header className="spacing-sm">
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light">
+            <div className="container-fluid">
+              <BrandIcon />
+              <div className="collapse navbar-collapse flex-grow-0">
+                <ul className="navbar-nav ml-auto">
+                  <li className={`nav-item${getNavLinkClass("/")}`}>
+                    <Button className="nav-link" type="link" href="/">
+                      Home
+                    </Button>
+                  </li>
+                  <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
+                    <Button className="nav-link" type="link" href="browse-by">
+                      Browse by
+                    </Button>
+                  </li>
+                  <li className={`nav-item${getNavLinkClass("/stories")}`}>
+                    <Button className="nav-link" type="link" href="/stories">
+                      Stories
+                    </Button>
+                  </li>
+                  <li className={`nav-item${getNavLinkClass("/agents")}`}>
+                    <Button className="nav-link" type="link" href="/agents">
+                      Agents
+                    </Button>
+                  </li>
+                </ul>
+              </div>
             </div>
-          </div>
-        </nav>
-      </div>
-    </header>
+          </nav>
+        </div>
+      </header>
+    </Fade>
   );
 }
